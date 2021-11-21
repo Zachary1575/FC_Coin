205f6d15004c8989ea742ce402a0a1a8623b536a
Zachary <75510412+Zachary1575@users.noreply.github.com>
2021-11-21 16:32:30 -0500

    Update App.js

--- client/web/src/App.js
+++ client/web/src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
 import Navbar from './components/pages/Navbar';
-import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
     <Router>
       <Navbar />
-      <Switch>
+      <Routes>
         <Route path='/' />
-      </Switch>
+      </Routes>
     </Router>
   );
 }
