ce6110f2a56607af1b55118f7cf450d0fd1a255e
Zachary <75510412+Zachary1575@users.noreply.github.com>
2021-11-21 17:17:07 -0500

    New Button changes (added Buttons)

--- client/web/src/components/pages/Navbar.js
+++ client/web/src/components/pages/Navbar.js
@@ -1,9 +1,27 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { MdFingerprint } from 'react-icons/md'
-import { FaBars, FaTimes } from 'react-icons/fa'
+import React from 'react';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { MdFingerprint } from 'react-icons/md';
+import { FaBars, FaTimes } from 'react-icons/fa';
+import { Button } from '../../Button';
 
 export default function Navbar() {
+    const [click, setClick] = useState(false);
+    const [button, setButton] = useState(true);
+
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false)
+
+    const showButton = () => {
+        if (window.innerWidth <= 960) {
+            setButton(false)
+        } else {
+            setButton(true)
+        }
+    };
+
+    window.addEventListener('resize', showButton);
+
     return (
         <>
             <div classname="navbar" /*This is the entire navbar element*/>
@@ -12,12 +30,37 @@ export default function Navbar() {
                         <MdFingerprint to='/' className = 'navbar-icon' />
                         FIDEICOMMISSUM
                     </Link>
-                    <div className ="menu-icon">
-                        asd
-                    </div>
+                    <div className ="menu-icon" onClick={handleClick} />
+                    {click ? <FaTimes /> : <FaBars />}
+                    
+                    <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                        <li className='nav-item'>
+                            <Link to='/About' className='nav-links'>
+                                About
+                            </Link>
+                        </li>
+
+                        <li className='nav-item'>
+                            <Link to='/ContactUs' className='nav-links'>
+                                Contact Us
+                            </Link>
+                        </li>
+                        
+                        <li className="nav-btn">
+                            {button ? (
+                                <Link to="/sign-up" className= "btn-link">
+                                    <Button buttonStyle='btn--outline'>SIGN UP</Button>
+                                </Link>
+                            ): (
+                                <Link to="/sign-up"  className= "btn-link">
+                                    <Button buttonStyle="btn--outline" buttonSize='btn--mobile'>SIGN UP</Button>
+                                </Link>
+                            )}
+
+                        </li>
+                    </ul>
                 </div>
             </div>
-            
         </>
-    )
+    );
 }
