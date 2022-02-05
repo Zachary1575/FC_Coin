ee1d1cc7940a89e5fd9451ac1aded260caf19dba
Zachary <75510412+Zachary1575@users.noreply.github.com>
2022-02-05 00:29:05 -0500

    Added Footer Content

--- client/src/Pages/HomePage/Footer/Footer.jsx
+++ client/src/Pages/HomePage/Footer/Footer.jsx
@@ -11,22 +11,62 @@ class footer extends Component {
                         <div className='footer-container'>
 
                             <div className='footer-box1'>
-                                <h1>Find Us</h1>
-                                <p>`Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.`</p>
+                                <div className='header'>
+                                    <h1 className='footer_h'>Find Us</h1>
+                                </div>
+                                <p className='footer_txt'>`Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.`</p>
                             </div>
 
                             <div className='footer-box2'>
-                                <h1>Find Us</h1>
-                                <p>`Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.`</p>
+                                <div className='header'>
+                                    <h1 className='footer_h'>Quick Links</h1>
+                                </div>
+                                
+                                <ul className='Quick_Links'>
+                                    <li className='link'>Link 1</li>
+                                    <li className='link'>Link 2</li>
+                                    <li className='link'>Link 3</li>
+                                    <li className='link'>Link 4</li>
+                                    <li className='link'>Link 5</li>
+                                </ul>
                             </div>
 
                             <div className='footer-box3'>
-                                <h1>Find Us</h1>
-                                <p>`Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.`</p>
+                                <div className='header'>
+                                    <h1 className='footer_h'>Follow Us</h1>
+                                </div>
+
+                                <ul className='Quick_Links'>
+                                <p className='footer_txt'>`Lorem ipsum dolor sit amet, consectetur adipiscing elit.`</p>
+                                    <li className='link'>Link 1</li>
+                                    <p className='footer_txt'>`Lorem ipsum dolor sit amet, consectetur adipiscing elit.`</p>
+                                    <li className='link'>Link 2</li>
+                                    <p className='footer_txt'>`Lorem ipsum dolor sit amet, consectetur adipiscing elit.`</p>
+                                    <li className='link'>Link 3</li>
+                                </ul>
                             </div>
-                            
                         </div>
                     </footer>
+                </div>
+
+                <div className='End'>
+                <div className='footer_end'>
+                    <ul className='footer_link_end'>
+                        <li className='footer_end_li'>Home</li>
+                        <li className='footer_end_li'>Community</li>
+                        <li className='footer_end_li'>About</li>
+                        <li className='footer_end_li'>Login</li>
+                        <li className='footer_end_li'>Sign-Up</li>
+                        <li className='footer_end_li'>Github</li>
+                    </ul>
+                    <p className='footer_end_text'>Copyright @2021 | Designed by Zachary1575</p> 
+                    <ul className='footer_socials_end'>
+                    <li className='footer_end_s'>LINK</li>
+                    <li className='footer_end_s'>LINK</li>
+                    <li className='footer_end_s'>LINK</li>
+                    <li className='footer_end_s'>LINK</li>
+                    </ul>
+                </div>
                 </div>
             </React.Fragment>
         );
